883d034c41d1283f64eb10d5ca152d528753e6d9
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-12-27 14:16:59 -0500

    add Visualización condicional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  const seleccionar = thisInput => {
+    setIsChecked(thisInput.checked);
+  };
+  return (
+    <div className="wrapper">
+      <label>
+        <input type="checkbox" onChange={e => seleccionar(e.target)} /> Mostrar
+        información importante
+      </label>
+      {isChecked ? (
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default App;
